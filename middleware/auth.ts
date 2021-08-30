b7985b594cb3bde38287fc0a2ee2e396a0bad8cf
nozomi-nakamitsu <nozomi_nakamitsu@arsaga.jp>
2021-08-30 10:09:02 +0900

    一旦コミット

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,31 +1,40 @@
-import { Middleware } from '@nuxt/types'
+import { Context, Middleware } from '@nuxt/types'
 import firebase, { firestore } from '@/plugins/firebase'
-const middleware: Middleware = ({ route, store, redirect }) => {
-  store.commit('setIsLoading', true)
-  console.log('store.getters.getIsLoading', store.getters.getIsLoading)
+const middleware: Middleware = ({ route,store, redirect, } :Context) => {
+  // store.commit('setIsLoading', true)
+  // console.log('store.getters.getIsLoading', store.getters.getIsLoading)
+
   firebase.auth().onAuthStateChanged((user: any) => {
+    console.log("user",user)
     if (user) {
       // ログイン中の場合の処理
       store.commit('setIsLogined', true)
-      firestore
-        .collection('users')
-        .doc(user.uid)
-        .get()
-        .then((doc) => {
-          store.commit('setCurrentUser', doc.data())
-          if (route.name === 'login') {
-            // return redirect('/')
-            window.location.href = '/'
-            return new Promise((resolve) => {
-              // Wait for broswer to redirect...
-            })
-          }
-        })
+      store.commit('setCurrentUser', {
+        token: user.refreshToken,
+        uid:user.uid,
+        photoURL: user.photoURL,
+        displayName: user.displayName,
+        nickName: user.displayName,
+      })
+      if (route.name === 'login') {
+     
+         redirect('/') 
+        
+     
+       
+      }
     } else if (!user && route.name !== 'login') {
-      return redirect('/login')
+     redirect('/login') 
+     
+     
+      
+
     }
   })
-  store.commit('setIsLoading', false)
-  console.log('store.getters.getIsLoading2', store.getters.getIsLoading)
+
+
+  // store.commit('setIsLoading', false)
+  // console.log('store.getters.getIsLoading2', store.getters.getIsLoading)
 }
 export default middleware
+
