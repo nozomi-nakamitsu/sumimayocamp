e3d580751c3a857c4c3a90cfb47ea6eea7f9458f
nozomi-nakamitsu <nozomi_nakamitsu@arsaga.jp>
2021-09-10 10:28:14 +0900

    今週のキャンプ画面作成

---
+++ components/organisms/TheDeclaration.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="declaration-container">
+    <h1>今週のキャンプ</h1>
+    <p>すみまよキャンプを完成させる！！！！</p>
+    <v-btn depressed> 宣言する </v-btn>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {},
+
+  setup() {
+    return {}
+  },
+})
+</script>
