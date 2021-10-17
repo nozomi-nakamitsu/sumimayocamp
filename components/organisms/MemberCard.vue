a5eb586a1afe54f0bb60e242171723d30bcf3b88
nozomi-nakamitsu <nozomi_nakamitsu@arsaga.jp>
2021-10-17 16:10:46 +0900

    feat:メンバーカードをひっくり返るように修正

--- components/organisms/MemberCard.vue
+++ components/organisms/MemberCard.vue
@@ -1,13 +1,39 @@
 <template>
-  <div class="member-card-container">
-    <div class="image">
-      <img :src="user.photoURL" alt="" class="img" />
-    </div>
-    <div class="body">
-      <p class="name">{{ user.nickName }}</p>
-      <div>
-        <div><Icon /></div>
-        <p>ログイン中</p>
+  <div class="flip-card">
+    <div class="wrapper">
+      <div class="cols">
+        <div class="col" ontouchstart="this.classList.toggle('hover');">
+          <div class="container">
+            <div class="front">
+              <div class="inner -front">
+                <div class="image">
+                  <img :src="user.photoURL" alt="" class="img" />
+                </div>
+                <div class="body">
+                  <p class="name">{{ user.nickName }}</p>
+                  <div>
+                    <div><Icon /></div>
+                    <p>ログイン中</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="back">
+              <div class="inner">
+                <div class="body">
+                  <p class="text">
+                    <span class="name">{{ user.nickName }}</span
+                    >さんの
+                  </p>
+                  <p class="text">今週の宣言</p>
+                  <p class="text -mt20">
+                    {{ declaration ? declaration : '宣言してないようです...' }}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -23,6 +49,10 @@ export default defineComponent({
     user: {
       type: Object,
       required: true,
+    },
+    declaration: {
+      type: String,
+      default: '',
     },
   },
   setup() {
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -6,17 +6,24 @@
     </div>
     <div class="member-list-container">
       <div v-for="user in allUsers" :key="user.uid">
-        <MemberCard :user="user" />
+        <MemberCard :user="user" :declaration="declaration(user.uid)" />
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onBeforeMount, ref } from '@nuxtjs/composition-api'
+import {
+  defineComponent,
+  onBeforeMount,
+  ref,
+  onBeforeUnmount,
+  useStore,
+  computed,
+} from '@nuxtjs/composition-api'
 import MemberCard from '@/components/organisms/MemberCard.vue'
-import { UsersRef } from '@/utilities/useFirestore'
-import { User } from '@/utilities/useConverter'
+import { UsersRef, DeclarationRef } from '@/utilities/useFirestore'
+import { User, Declaration } from '@/utilities/useConverter'
 
 export default defineComponent({
   components: {
@@ -24,18 +31,50 @@ export default defineComponent({
   },
   setup() {
     const allUsers = ref<User[]>([])
+    const store = useStore()
+    const declarations = ref<Declaration[]>([])
+    let unsubscribeDeclaration = null as any
     // ユーザー一覧データを取得する
     onBeforeMount(() => {
       UsersRef()
         .get()
-        .then(function (querySnapshot) {
+        .then((querySnapshot) => {
           querySnapshot.forEach(function (doc) {
             allUsers.value = [...allUsers.value, doc.data()]
           })
         })
+      getDeclaration()
     })
+    const getDeclaration = () => {
+      unsubscribeDeclaration = DeclarationRef().onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach(
+          (change) => {
+            declarations.value = [...declarations.value, change.doc.data()]
+          },
+          (error: any) => {
+            store.dispatch('onRejected', error)
+          }
+        )
+      })
+    }
+    const declaration = computed(() => (uid: string) => {
+      const target = declarations.value.find(
+        (declaration: Declaration) => declaration.uid === uid
+      )
+      if (target) {
+        return target.declaration
+      }
+      return ''
+    })
+
+    // ページ遷移後にsnapshotでの監視をstopする
+    onBeforeUnmount(() => {
+      unsubscribeDeclaration()
+    })
+
     return {
-      allUsers
+      allUsers,
+      declaration,
     }
   },
 })
