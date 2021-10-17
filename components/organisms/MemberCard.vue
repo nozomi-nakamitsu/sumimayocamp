4dd45342e55ae48c63f380c8d7b608e70b260b40
nozomi-nakamitsu <nozomi_nakamitsu@arsaga.jp>
2021-10-17 14:39:19 +0900

    メンバーリストUI作成

---
+++ components/organisms/MemberCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="member-card-container">
+    <div class="image">
+      <img :src="user.photoURL" alt="" class="img" />
+    </div>
+    <div class="body">
+      <p class="name">{{ user.nickName }}</p>
+      <div>
+        <div><Icon /></div>
+        <p>ログイン中</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import Icon from '../molecules/Icon.vue'
+
+export default defineComponent({
+  components: { Icon },
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    return {}
+  },
+})
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -3,19 +3,40 @@
     <div class="title">
       <p class="text">Members List</p>
       <div class="line"></div>
-      <p class="comment">準備中！！綺麗になるのをお楽しみに〜</p>
     </div>
-    <div class="main"></div>
+    <div class="member-list-container">
+      <div v-for="user in allUsers" :key="user.uid">
+        <MemberCard :user="user" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, onBeforeMount, ref } from '@nuxtjs/composition-api'
+import MemberCard from '@/components/organisms/MemberCard.vue'
+import { UsersRef } from '@/utilities/useFirestore'
+import { User } from '@/utilities/useConverter'
 
 export default defineComponent({
-  components: {},
+  components: {
+    MemberCard,
+  },
   setup() {
-    return {}
+    const allUsers = ref<User[]>([])
+    // ユーザー一覧データを取得する
+    onBeforeMount(() => {
+      UsersRef()
+        .get()
+        .then(function (querySnapshot) {
+          querySnapshot.forEach(function (doc) {
+            allUsers.value = [...allUsers.value, doc.data()]
+          })
+        })
+    })
+    return {
+      allUsers
+    }
   },
 })
 </script>
